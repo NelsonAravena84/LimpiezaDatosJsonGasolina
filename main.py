d2a0de92c693f7916dbb39d47dcd0f63f1bdac44
Nelson Aravena <nelsonea2024@gmail.com>
2025-01-10 18:04:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ url = "https://api.bencinaenlinea.cl/api/estaciones/precios_combustibles/2,10/re
 
 #el token que te proporcionan
 
-token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vYXBpLmNuZS5jbC9hcGkvbG9naW4iLCJpYXQiOjE3MzY1NDEyMTEsImV4cCI6MTczNjU0NDgxMSwibmJmIjoxNzM2NTQxMjExLCJqdGkiOiJNa0ZrbjZyWndyZHZZWkdUIiwic3ViIjoiMjU0MyIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.yrr-2Fb_Cl0N8IvtMsRwUYhDnSBYaXWg2JslE8d2Tbk"
+token = ""
 
 #configurar encabezados para el token
 
