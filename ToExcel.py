ca22240fffdc49b9d9687c78d5e20862dd68f05d
Nelson Aravena <nelsonea2024@gmail.com>
2025-01-11 18:00:30 -0300

    "optimización codigo"

--- ToExcel.py
+++ ToExcel.py
@@ -1,5 +1,6 @@
 import json
 import pandas as pd 
+import openpyxl 
 
 f = open('data.json', 'r')
 data = json.load(f)
@@ -9,39 +10,65 @@ df = pd.json_normalize(data, record_path=["data"])
 #contarCopec = df['marca_nombre'].value_counts().get('ARAMCO', 0)
 #print(contarCopec)
 
-unique_names = df['marca_nombre'].unique()
-print(unique_names)
+#unique_names = df['marca_nombre'].unique()
+#print(unique_names)
 
 #limpieza de Datos para COPEC considerar que se esta usando combustible: Gasolina 97
 #ademas que este dataset contiene los elementos de la región metropolitana 
-dfCopec = df.drop(['estacion_servicio_id','tipo_atencion','estacion_servicio_codigo','en_mantencion','comuna_nombre','comuna_id','estacion_direccion','region_nombre','region_id'], axis = 1)
-dfCopec = dfCopec[dfCopec['marca_nombre'] == 'COPEC']
-dfCopec = dfCopec.reset_index(drop=True) #importante que se actualice el indice del dataset
+#dfCopec = df.drop(['estacion_servicio_id','tipo_atencion','estacion_servicio_codigo','en_mantencion','comuna_nombre','comuna_id','estacion_direccion','region_nombre','region_id'], axis = 1)
+#dfCopec = dfCopec[dfCopec['marca_nombre'] == 'COPEC']
+#dfCopec = dfCopec.reset_index(drop=True) #importante que se actualice el indice del dataset
 
 
 #limpieza de Datos para SHELL 
-dfShell = df.drop(['estacion_servicio_id','tipo_atencion','estacion_servicio_codigo','en_mantencion','comuna_nombre','comuna_id','estacion_direccion','region_nombre','region_id'], axis = 1)
-dfShell = dfShell[dfShell['marca_nombre'] == 'SHELL']
-dfShell = dfShell.reset_index(drop=True)
+#dfShell = df.drop(['estacion_servicio_id','tipo_atencion','estacion_servicio_codigo','en_mantencion','comuna_nombre','comuna_id','estacion_direccion','region_nombre','region_id'], axis = 1)
+#dfShell = dfShell[dfShell['marca_nombre'] == 'SHELL']
+#dfShell = dfShell.reset_index(drop=True)
 
 
 #limpieza de Datos para Petrobras 
-dfPetrobras = df.drop(['estacion_servicio_id','tipo_atencion','estacion_servicio_codigo','en_mantencion','comuna_nombre','comuna_id','estacion_direccion','region_nombre','region_id'], axis = 1)
-dfPetrobras = dfPetrobras[dfPetrobras['marca_nombre'] == 'PETROBRAS']
-dfPetrobras = dfPetrobras.reset_index(drop=True)
+#dfPetrobras = df.drop(['estacion_servicio_id','tipo_atencion','estacion_servicio_codigo','en_mantencion','comuna_nombre','comuna_id','estacion_direccion','region_nombre','region_id'], axis = 1)
+#dfPetrobras = dfPetrobras[dfPetrobras['marca_nombre'] == 'PETROBRAS']
+#dfPetrobras = dfPetrobras.reset_index(drop=True)
 
 
 #limpieza de datos para JLC
-dfJLC = df.drop(['estacion_servicio_id','tipo_atencion','estacion_servicio_codigo','en_mantencion','comuna_nombre','comuna_id','estacion_direccion','region_nombre','region_id'], axis = 1)
-dfJLC = dfJLC[dfJLC['marca_nombre'] == 'JLC']
-dfJLC = dfJLC.reset_index(drop=True)
+#dfJLC = df.drop(['estacion_servicio_id','tipo_atencion','estacion_servicio_codigo','en_mantencion','comuna_nombre','comuna_id','estacion_direccion','region_nombre','region_id'], axis = 1)
+#dfJLC = dfJLC[dfJLC['marca_nombre'] == 'JLC']
+#dfJLC = dfJLC.reset_index(drop=True)
 
-frames = [dfCopec, dfJLC, dfPetrobras, dfShell]
-nuevodata = pd.concat(frames)
-nuevodata = nuevodata.reset_index(drop=True)
+#frames = [dfCopec, dfJLC, dfPetrobras, dfShell]
+#nuevodata = pd.concat(frames)
+#nuevodata = nuevodata.reset_index(drop=True)
+
+def LimpiezaDatos(dataframe, marca):
+    eliminandocolumnas = dataframe.drop(['estacion_servicio_id','tipo_atencion','estacion_servicio_codigo','en_mantencion','comuna_nombre','comuna_id','estacion_direccion','region_nombre','region_id'], axis = 1)
+    dataframemarca = eliminandocolumnas[eliminandocolumnas['marca_nombre'] == marca]
+    dataframemarca = dataframemarca.reset_index(drop=True)
+
+    return (dataframemarca)
 
 
+def unionDatos(DatosShell, DatosCopec, DatosPetrobras, DatosJLC):
+    frames = [DatosShell,DatosCopec, DatosPetrobras, DatosJLC]
+    dataframefinal = pd.concat(frames)
+    dataframefinal = dataframefinal.reset_index(drop=True)
 
-nuevodata.to_excel("output.xlsx")
+    return (dataframefinal);
+
+
+#PRIMERA FUNCIÓN
+DatosShell = LimpiezaDatos(df, "SHELL");
+DatosCopec = LimpiezaDatos(df, "COPEC"); 
+DatosPetrobras = LimpiezaDatos (df, "PETROBRAS");
+DatosJLC = LimpiezaDatos (df, "JLC");
+
+#SEGUNDA FUNCIÓN
+DataframeNuevoFinal = unionDatos(DatosShell, DatosCopec, DatosPetrobras, DatosJLC);
+DataframeNuevoFinal.to_excel('output.xlsx', index=False)
+
+print(DataframeNuevoFinal);
+
+
 
 
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ url = "https://api.bencinaenlinea.cl/api/estaciones/precios_combustibles/2,10/re
 
 #el token que te proporcionan
 
-token = ""
+token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vYXBpLmNuZS5jbC9hcGkvbG9naW4iLCJpYXQiOjE3MzY2MjM0MzQsImV4cCI6MTczNjYyNzAzNCwibmJmIjoxNzM2NjIzNDM0LCJqdGkiOiJibWpoTWM5OHN6U3h4R1ZTIiwic3ViIjoiMjU0MyIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.A4SYmhnSqMY1aU4u83lxxls_y2rrhsyOjAOuXgJcW6g"
 
 #configurar encabezados para el token
 
