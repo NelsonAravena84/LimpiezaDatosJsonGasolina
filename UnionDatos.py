166bf36d4e3eeaca73a84e72db1a3249f6199832
Nelson Aravena <nelsonea2024@gmail.com>
2025-01-22 18:25:51 -0300

    "Reorganización Codigo"

---
+++ UnionDatos.py
@@ -0,0 +1,49 @@
+import json 
+import pandas as pd 
+import openpyxl
+
+def LeyendoArchivos(NombreArchivo, NumeroArchivo):
+    f = open(f'Datos\{NombreArchivo}{NumeroArchivo}.json', 'r')
+    data = json.load(f)
+    df = pd.json_normalize(data, record_path=["data"])
+    return df
+
+#Datos Gasolina 93
+RegionMetropolitana93 = LeyendoArchivos("Metropolitana",93)
+RegionMagallanes93 = LeyendoArchivos("Magallanes",93)
+RegionValparaiso93 = LeyendoArchivos("Valparaiso",93)
+RegionArica93 = LeyendoArchivos("Arica",93)
+
+#Datos Gasolina 95
+RegionMetropolitana95 = LeyendoArchivos("Metropolitana",95)
+RegionMagallanes95 = LeyendoArchivos("Magallanes",95)
+RegionValparaiso95 = LeyendoArchivos("Valparaiso",95)
+RegionArica95 = LeyendoArchivos("Arica",95)
+
+#Datos Gasolina 97
+RegionMetropolitana97 = LeyendoArchivos("Metropolitana",97)
+RegionMagallanes97 = LeyendoArchivos("Magallanes",97)
+RegionValparaiso97 = LeyendoArchivos("Valparaiso",97)
+RegionArica97 = LeyendoArchivos("Arica",97)
+
+#Datos Diesel 
+RegionMetropolitanaDiesel = LeyendoArchivos("Metropolitana","Diesel")
+RegionMagallanesDiesel = LeyendoArchivos("Magallanes","Diesel")
+RegionValparaisoDiesel = LeyendoArchivos("Valparaiso","Diesel")
+RegionAricaDiesel = LeyendoArchivos("Arica","Diesel")
+
+#print(RegionAricaDiesel)
+
+def JuntandoDatos(Number,RegionMetropolitana,RegionMagallanes,RegionValparaiso,RegionArica):
+    df = [RegionMetropolitana, RegionMagallanes,RegionValparaiso, RegionArica]
+    tipoCombustible = (f'Gasolina {Number}')
+    dataframeFinal= pd.concat(df)
+    dataframeFinal['Tipo_combustible'] = tipoCombustible
+    return dataframeFinal
+
+dataframeGasolina93 = JuntandoDatos(93,RegionMetropolitana93,RegionMagallanes93,RegionValparaiso93,RegionArica93)
+dataframeGasolina95 = JuntandoDatos(95,RegionMetropolitana95,RegionMagallanes95,RegionValparaiso95,RegionArica95)
+#dataframeGasolina97 = JuntandoDatos(RegionMetropolitana97,RegionMagallanes97,RegionValparaiso97,RegionArica97)
+#dataframeGasolinaDiesel = JuntandoDatos(RegionMetropolitanaDiesel,RegionMagallanesDiesel,RegionValparaisoDiesel,RegionAricaDiesel)
+
+print(dataframeGasolina95.head())
